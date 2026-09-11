6081c29823d0f81e4238efd2fb9007c4e12fb3b1
irrwin <irwinsim01@gmail.com>
2026-09-11 22:39:30 +0800

    ex2v2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -2,13 +2,21 @@
     import { ref } from 'vue'
 
     // Add code here
-    const image = ref('/public/assets/me.png')
-    const width = ref(300)
-    const height = ref(300)
+    const image = ref('/assets/me.png')
+    const width = ref(200)
+    const height = ref(200)
+
+    const myObj = ref({
+        width: 200,
+        height: 200,
+        src: 'assets/me.png'
+    })
 </script>
 
 <template>
 
     <!-- Add code here -->
-   <img v-bind:src="image" :width="width" :height="height">
+   <img v-bind:src="image" :width="width" :height="height"/>
+
+   <img v-bind="myObj"/>
 </template>
