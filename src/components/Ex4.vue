5cf309342ed52854cd76fcc1eafefee6f7025c63
irrwin <irwinsim01@gmail.com>
2026-09-12 21:32:15 +0800

    ex4v2

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -8,6 +8,7 @@
     const blueBtn = ref(true)
 
     // part 2
+    const id2 = ref("demo2")
     const activeColor = ref('red')
     const redBtn = ref(true)
 
@@ -57,7 +58,7 @@
 
     <div id="part2">
         <div :style="{color: activeColor}">
-            div ID : {{id}} 
+            div ID : {{id2}} 
         </div>
        
         <button type="button" v-on:click="changeTextColor" :class="{
